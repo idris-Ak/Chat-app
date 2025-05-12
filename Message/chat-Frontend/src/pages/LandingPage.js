982985fd22f5b1e8c63767c45047231a07e801dc
idris-Ak <132613240+idris-Ak@users.noreply.github.com>
2025-05-12 13:58:34 +1000

    Update LandingPage component and refine CSS styles for CHAT responsiveness and layout

--- Message/chat-Frontend/src/pages/LandingPage.js
+++ Message/chat-Frontend/src/pages/LandingPage.js
@@ -2,7 +2,6 @@ import { useState, useEffect } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 
 export default function LandingPage() {
-
-    
+    <h1>LandingPage</h1>
 
 }
